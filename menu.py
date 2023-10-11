3387156fcf79831cad077de5493411bfa48b69fa
Andrew Phommathep <>
2023-10-11 14:10:00 +0800

    Refactor with services, builders, dependency injections, and decorators

--- menu.py
+++ menu.py
@@ -1,32 +1,42 @@
+import json
 from fastapi import APIRouter
 from models.menu import Menu
+from typing import List
 
 menu_router = APIRouter()
 
-menu_items = {
-    "items": 
-    [
-        {
-            "id": "781d66c4-87aa-469f-88df-1117c77fb576",
-            "name": "Brooklyn Spaghetti",
-            "genre": "ITALIAN",
-            "price": 14.99
-        },
-        {
-            "id": "b9024fb1-c3d9-4c5f-b447-0826e86988b4",
-            "name": "Lamb Gyro",
-            "genre": "GREEK",
-            "price": 12.99
-        },
-        {
-            "id": "f4f81a55-8f3d-4ae1-ae80-c81f51c0c49c",
-            "name": "Cheeseburger",
-            "genre": "AMERICAN",
-            "price": 9.99
-        }
-    ]
-}
+class MenuItem:
+    def __init__(self, id, name, genre, price):
+        self.id = id
+        self.name = name
+        self.genre = genre
+        self.price = price
+
+class MenuService:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(MenuService, cls).__new__(cls)
+            cls._instance.init()
+        return cls._instance
+
+    def init(self):
+        self.menu_items = [
+            MenuItem("781d66c4-87aa-469f-88df-1117c77fb576", "Brooklyn Spaghetti", "ITALIAN", 14.99),
+            MenuItem("b9024fb1-c3d9-4c5f-b447-0826e86988b4", "Lamb Gyro", "GREEK", 12.99),
+            MenuItem("f4f81a55-8f3d-4ae1-ae80-c81f51c0c49c", "Cheeseburger", "AMERICAN", 9.99),
+        ]
+
+    def get_menu_items(self) -> List[MenuItem]:
+        return self.menu_items
+
+# Initialize the MenuService
+menu_service = MenuService()
 
 @menu_router.get("/", response_model=Menu)
 async def get_menu():
-    return menu_items
+    menu_items = menu_service.get_menu_items()
+    menu_items_dicts = [menu.__dict__ for menu in menu_items]
+    
+    return {"items": menu_items_dicts}
--- order.py
+++ order.py
@@ -1,88 +1,115 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from models.order import OrderReceipt, OrderRequest, OrderResponse
-from menu import menu_items
+from menu import MenuService
 import uuid
 
 # Create an APIRouter
 order_router = APIRouter()
 
-# Mock data for order
-orders = {
-    "c424989e-6760-45bd-bacd-dad9a4e9efd6" : {
-        "id": "c424989e-6760-45bd-bacd-dad9a4e9efd6",
-        "status": "COMPLETE",
-        "total": 70.94,
-        "items": [
-            {
-                "id": "781d66c4-87aa-469f-88df-1117c77fb576",
-                "price": 14.99,
-                "quantity": 1
-            },
-            {
-                "id": "b9024fb1-c3d9-4c5f-b447-0826e86988b4",
-                "price": 12.99,
-                "quantity": 2
-            },
-            {
-                "id": "f4f81a55-8f3d-4ae1-ae80-c81f51c0c49c",
-                "price": 9.99,
-                "quantity": 3
-            }
-        ]
-    }
-}
+class OrderItem:
+    def __init__(self, id, price, quantity):
+        self.id = id
+        self.price = price
+        self.quantity = quantity
 
-# Function to create an order
-@order_router.post("/", response_model=OrderResponse)
-async def create_order(order: OrderRequest):
-    calculated_total = 0
-    ordered_items = []
+class Order:
+    def __init__(self, id, status, total, items):
+        self.id = id
+        self.status = status
+        self.total = total
+        self.items = [OrderItem(**item_data) for item_data in items]
 
-    for item in order.items:
-        for menu_item in menu_items["items"]:
-            if item.id == menu_item["id"]:
-                menu_item_found = menu_item
+
+class OrderService:
+    def __init__(self, menu_service: MenuService):
+        self.menu_service = menu_service
         
-        if not menu_item_found:
-            raise HTTPException(status_code=400, detail="Invalid id")
+        # Mock data for order
+        order_data = {
+            "c424989e-6760-45bd-bacd-dad9a4e9efd6" : {
+                "id": "c424989e-6760-45bd-bacd-dad9a4e9efd6",
+                "status": "COMPLETE",
+                "total": 70.94,
+                "items": [
+                    {
+                        "id": "781d66c4-87aa-469f-88df-1117c77fb576",
+                        "price": 14.99,
+                        "quantity": 1
+                    },
+                    {
+                        "id": "b9024fb1-c3d9-4c5f-b447-0826e86988b4",
+                        "price": 12.99,
+                        "quantity": 2
+                    },
+                    {
+                        "id": "f4f81a55-8f3d-4ae1-ae80-c81f51c0c49c",
+                        "price": 9.99,
+                        "quantity": 3
+                    }
+                ]
+            }
+        }
+        self.orders = {order_id: Order(**order_data) for order_id, order_data in order_data.items()}
 
-        total_price = menu_item_found["price"] * item.quantity
-        calculated_total += total_price
+    # Function to create an order
+    def create_order(self, order: OrderRequest):
+        calculated_total = 0
+        ordered_items = []
 
-        ordered_items.append({
-            "id": menu_item_found["id"],
-            "price": menu_item_found["price"],
-            "quantity": item.quantity,
-        })
+        for item in order.items:
+            menu_item_found = None
+            for menu_item in self.menu_service.get_menu_items():
+                if item.id == menu_item.id:
+                    menu_item_found = menu_item
 
-    id = str(uuid.uuid4())
+            if not menu_item_found:
+                raise HTTPException(status_code=400, detail="Invalid id")
 
-    new_order = {
-        "order_id": id,
-        "status": "IN-PROGRESS",
-        "total": calculated_total,
-    }
-    
-    orders[id] = {
-        "id": id,
-        "status": "IN-PROGRESS",
-        "total": calculated_total,
-        "items": ordered_items
-    }
+            total_price = menu_item_found.price * item.quantity
+            calculated_total += total_price
 
-    return new_order
+            # Create an OrderItem instance directly from the 'item' variable
+            ordered_items.append(OrderItem(id=item.id, price=menu_item_found.price, quantity=item.quantity))
 
-# Function to get an order receipt
-@order_router.get("/{id}", response_model=OrderReceipt)
-async def get_order_receipt(id: str):
-    order = orders[id] if id in orders else None
-    
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
+        id = str(uuid.uuid4())
 
-    return {
-        "id": order["id"],
-        "status": order["status"],
-        "total": order["total"],
-        "items": order["items"]
-    }
+        new_order = {
+            "order_id": id,
+            "status": "IN-PROGRESS",
+            "total": calculated_total,
+        }
+
+        self.orders[id] = {
+            "id": id,
+            "status": "IN-PROGRESS",
+            "total": calculated_total,
+            "items": ordered_items
+        }
+
+        return new_order
+
+    # Function to get an order receipt
+    def get_order_receipt(self, order_id: str):
+        order = self.orders.get(order_id)
+        
+        if order is None:
+            raise HTTPException(status_code=404, detail="Order not found")
+
+        return {
+            "id": order.id,
+            "status": order.status,
+            "total": order.total,
+            "items": [order_item.__dict__ for order_item in order.items]
+        }
+        
+menu_service = MenuService()
+
+order_service = OrderService(menu_service)
+
+@order_router.post("/", response_model=OrderResponse)
+async def create_order(order: OrderRequest, order_service: OrderService = Depends(lambda: order_service)):
+    return order_service.create_order(order)
+
+@order_router.get("/{order_id}", response_model=OrderReceipt)
+async def get_order_receipt(order_id: str, order_service: OrderService = Depends(lambda: order_service)):
+    return order_service.get_order_receipt(order_id)
